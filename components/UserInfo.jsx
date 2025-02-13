28b19b62f736f812608ead5c724af58a490f9135
JaneLabrador <labrador.janefaith111@gmail.com>
2025-02-13 15:00:12 +0800

    my changes

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
 export default function UserInfo() {
+    const { data: session } = useSession();
+
+
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="shadow-lg p-8 bg-zinc-300/10 flex flex-col gap-2 my-6 text-center rounded-lg">
                 <div>
-                    Name: <span className="font-bold">Jane</span>
+                    Name: <span className="font-bold">{session?.user?.name}</span>
                 </div>
                 <div>
-                    Email: <span className="font-bold">Jane@gmail.com</span>
+                    Email: <span className="font-bold">{session?.user?.email}</span>
                 </div>
-                <button className="bg-red-500 text-white font-bold px-6 py-2 mt-3 rounded">
+                <button onClick={() => signOut()}
+                 className="bg-red-500 text-white font-bold px-6 py-2 mt-3 rounded">
                     Log Out
                 </button>
             </div>
